f0516f45b766075ca363330c3e268d2d1a5e36b3
Valentine124 <kaluvalentine60@gmail.com>
2024-05-20 19:48:05 -0700

    Task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,32 +1,32 @@
 #!/usr/bin/python3
 
-"""
-Contains a script that
+"""Contains a script that
 make a request from an API
+
 and print the reponse to the 
 std output
 """
 
-
-import requests
-from sys import argv
-
-todo_req = requests.get(f'https://jsonplaceholder.typicode.com/todos?userId={argv[1]}')
-user_re = requests.get(f'https://jsonplaceholder.typicode.com/users/{argv[1]}')
-
-user = user_re.json()
-todos = todo_req.json()
-
-total_task = len(todos)
-task_done = 0
-titles = []
-
-for todo in todos:
-    if todo["completed"] == True:
-        task_done += 1
-        titles.append(todo["title"])
-
-print(f'Employee {user["name"]} is done with task({task_done}/{total_task}):')
-
-for title in titles:
-    print(f'\t {title}')
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+    
+    todo_req = requests.get(f'https://jsonplaceholder.typicode.com/todos?userId={argv[1]}')
+    user_re = requests.get(f'https://jsonplaceholder.typicode.com/users/{argv[1]}')
+    
+    user = user_re.json()
+    todos = todo_req.json()
+    
+    total_task = len(todos)
+    task_done = 0
+    titles = []
+    
+    for todo in todos:
+        if todo.get("completed") == True:
+            task_done += 1
+            titles.append(todo.get("title"))
+    
+    print(f'Employee {user.get("name")} is done with task({task_done}/{total_task}):')
+    
+    for title in titles:
+        print(f'\t {title}')
