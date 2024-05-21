5384df120549188091d8a2aec8ad25e6cff6576e
Valentine124 <kaluvalentine60@gmail.com>
2024-05-20 20:14:31 -0700

    Task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,31 +2,33 @@
 
 """Contains a script that
 make a request from an API
-
-and print the reponse to the 
+and print the reponse to the
 std output
 """
 
 if __name__ == "__main__":
     import requests
     from sys import argv
-    
-    todo_req = requests.get(f'https://jsonplaceholder.typicode.com/todos?userId={argv[1]}')
-    user_re = requests.get(f'https://jsonplaceholder.typicode.com/users/{argv[1]}')
-    
+
+    todo_req = requests.get(f'https://jsonplaceholder.typicode'
+                            f'.com/todos?userId={argv[1]}')
+    user_re = requests.get(f'https://jsonplaceholder.typicode.com'
+                           f'/users/{argv[1]}')
+
     user = user_re.json()
     todos = todo_req.json()
-    
+
     total_task = len(todos)
     task_done = 0
     titles = []
-    
+
     for todo in todos:
-        if todo.get("completed") == True:
+        if todo.get("completed") is True:
             task_done += 1
             titles.append(todo.get("title"))
-    
-    print(f'Employee {user.get("name")} is done with task({task_done}/{total_task}):')
-    
+
+    print(f'Employee {user.get("name")} is done '
+          f'with task({task_done}/{total_task}):')
+
     for title in titles:
         print(f'\t {title}')
